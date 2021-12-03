f1dd7c6ff47ecfaf994afd9fd7027e8a8feed910
Mauricio Cruz <34497377+MauricioCruzD@users.noreply.github.com>
2021-12-02 22:03:25 -0500

    Add files via upload

---
+++ src/controllers/Usuarios.controllers.js
@@ -0,0 +1,126 @@
+const UsuarioCtrl = {};
+
+const Usuario = require("../models/Usuarios.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+//Crear Usuario
+UsuarioCtrl.crearUsuario = async (req, res) => {
+  const {
+    nombre,
+    apellido,
+    email,
+    clave,
+    telefono,
+    direcion,
+    genero,
+    ciudad,
+    fcumpleanos,
+    fcreacion,
+    admin,
+  } = req.body;
+
+  const NuevoUsuario = new Usuario({
+    nombre,
+    apellido,
+    email,
+    clave,
+    telefono,
+    direcion,
+    genero,
+    ciudad,
+    fcumpleanos,
+    fcreacion,
+    admin,
+  });
+
+  const correoUsuario = await Usuario.findOne({ email: email });
+
+  if (correoUsuario) {
+    res.json({
+      mensaje: "El correo ya existe",
+    });
+  } else {
+    NuevoUsuario.clave = await bcrypt.hash(clave, 10);
+    const token = jwt.sign({ _id: NuevoUsuario._id }, "Secreta");
+    await NuevoUsuario.save();
+
+    res.json({
+      mensaje: "Bienvenido",
+      id: NuevoUsuario._id,
+      nombre: NuevoUsuario.nombre,
+      token,
+    });
+  }
+};
+
+//Listar Usuarios general
+UsuarioCtrl.listarUsuarios = async (req, res) => {
+  const respuesta = await Usuario.find();
+  res.json(respuesta);
+};
+
+//Listar Usuario x Id
+UsuarioCtrl.listarId = async (req, res) => {
+  const id = req.params.id;
+  const respuesta = await Usuario.findOne({ _id: id });
+  res.json(respuesta);
+};
+
+//Eliminar Usuario x Id
+UsuarioCtrl.eliminarUsuario = async (req, res) => {
+  const id = req.params.id;
+  await Usuario.findByIdAndRemove({ _id: id });
+  res.json({
+    mensaje: "Usuario Eliminado",
+  });
+};
+
+//Actualizar Usuario x Id
+UsuarioCtrl.actualizarUsuario = async (req, res) => {
+  const id = req.params.id;
+  await Usuario.findByIdAndUpdate({ _id: id }, req.body);
+  res.json({
+    mensaje: "Usuario Actualizado",
+  });
+};
+
+//Buscar Usuario
+UsuarioCtrl.buscarUsuario = async (req, res) => {
+  const { nombre } = req.params;
+  const respuesta = await Usuario.find({
+    nombre: { $regex: ".*" + nombre + ".*" },
+  });
+  res.json(respuesta);
+};
+
+//Login Usuario
+UsuarioCtrl.login = async (req, res) => {
+  const { email, clave } = req.body;
+  const usuario = await Usuario.findOne({ email: email });
+
+  if (!usuario) {
+    return res.json({
+      mensaje: "El Usuario no esta registrado",
+    });
+  }
+
+  const match = await bcrypt.compare(clave, usuario.clave);
+
+  if (match) {
+    const token = jwt.sign({ _id: usuario._id }, "Secreta");
+    res.json({
+      mensaje: "Iniciaste Sesion correctamente",
+      id: usuario.id,
+      nombre: usuario.nombre,
+      admin: usuario.admin,
+      token,
+    });
+  } else {
+    res.json({
+      mensaje: "Contraseña incorrecta",
+    });
+  }
+};
+
+module.exports = UsuarioCtrl;
---
+++ src/models/Usuarios.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+//Creacion de un Esquema
+
+const UsuariosSchema = new Schema({
+  nombre: String,
+  apellido: String,
+  email: String,
+  clave: String,
+  telefono: String,
+  direcion: String,
+  genero: String,
+  ciudad: String,
+  fcumpleanos: Date,
+  fcreacion: Date,
+  admin: Boolean,
+});
+
+//Formato Date: aaaa-mm-dd
+
+//Convertir a modelo
+module.exports = mongoose.model("Usuarios", UsuariosSchema);
